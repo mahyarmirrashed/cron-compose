f8a60227e6b014489f519aaffde0e1743d0296de
Mahyar Mirrashed <mah.mirr@gmail.com>
2023-11-09 01:15:57 -0600

    test: add human readable chain method tests

--- src/lib/CronComposer.spec.ts
+++ src/lib/CronComposer.spec.ts
@@ -93,6 +93,82 @@ describe("CronComposer", () => {
     });
   });
 
+  it("should add a single month to slot correctly", () => {
+    cronComposer.in("jan");
+    expect(cronComposer.toString()).toBe("* * * 1 *");
+  });
+
+  it("should add consecutive months to the slot correctly", () => {
+    cronComposer.in("jan", "feb", "mar");
+    expect(cronComposer.toString()).toBe("* * * 1-3 *");
+  });
+
+  it("should add multiple months to the slot correctly", () => {
+    cronComposer.in("jan", "mar");
+    expect(cronComposer.toString()).toBe("* * * 1,3 *");
+  });
+
+  it("should add a single day of the week to the slot correctly", () => {
+    cronComposer.on("sun");
+    expect(cronComposer.toString()).toBe("* * * * 0");
+  });
+
+  it("should add consecutive days of the week to the slot correctly", () => {
+    cronComposer.on("sun", "mon", "tue");
+    expect(cronComposer.toString()).toBe("* * * * 0-2");
+  });
+
+  it("should add multiple days of the week to the slot correctly", () => {
+    cronComposer.on("sun", "tue");
+    expect(cronComposer.toString()).toBe("* * * * 0,2");
+  });
+
+  it("should set a specific hour to the slot correctly", () => {
+    cronComposer.at(1);
+    expect(cronComposer.toString()).toBe("* 1 * * *");
+  });
+
+  it("should set a specific hour with an AM meridiem correctly", () => {
+    cronComposer.at(1, "am");
+    expect(cronComposer.toString()).toBe("* 1 * * *");
+  });
+
+  it("should set a specific hour with an PM meridiem correctly", () => {
+    cronComposer.at(1, "pm");
+    expect(cronComposer.toString()).toBe("* 13 * * *");
+  });
+
+  it("should set midnight correctly", () => {
+    cronComposer.at(12, "am");
+    expect(cronComposer.toString()).toBe("* 0 * * *");
+  });
+
+  it("should set noon correctly", () => {
+    cronComposer.at(12, "pm");
+    expect(cronComposer.toString()).toBe("* 12 * * *");
+  });
+
+  it("should add step value to each field correctly", () => {
+    cronComposer.enableSeconds();
+    cronComposer.every(1, "seconds");
+    cronComposer.every(1, "minutes");
+    cronComposer.every(1, "hours");
+    cronComposer.every(1, "days");
+    cronComposer.every(1, "months");
+    cronComposer.every(1, "weekdays");
+    expect(cronComposer.toString()).toBe("* * * * * *");
+  });
+
+  it("should add hour range correctly", () => {
+    cronComposer.from(6, "am").to(4, "pm");
+    expect(cronComposer.toString()).toBe("* 6-16 * * *");
+  });
+
+  it("should use except clause correctly", () => {
+    cronComposer.every(1, "hours").except.at(12, "pm");
+    expect(cronComposer.toString()).toBe("* 0-11,13-23 * * *");
+  });
+
   it("should parse a simple cron string correctly", () => {
     cronComposer.parse("* * * * *");
     expect(cronComposer.toString()).toBe("* * * * *");
